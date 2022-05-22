911afde100e920c6f4fe151cec8a82d93e049495
jrussell-OSU <jacob.r.russell@gmail.com>
2022-05-22 16:01:27 +0000

    POST and GET routers working

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ const datastore = new Datastore({
   projectId: 'api-portfolio-project-350723',
 });
 
+//Get credentials for data store
+process.env['GOOGLE_APPLICATION_CREDENTIALS'] = './secrets/datastore_auth.json';
+
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
@@ -59,9 +62,11 @@ const management_token_request = {
   }
 };
 
+const DEBUG = true;   //if true, shows debug if (DEBUG) console.logs
+
 //-----------------------FUNCTIONS-----------------------------------
 
-
+//get auth0 management token to use management API tools
 async function get_management_token(){
   const options = {
     method: "POST",
@@ -74,12 +79,13 @@ async function get_management_token(){
     }
   };
   let response = await axios.request(options);
-  console.log(response);
+  if (DEBUG) console.log(response);
   const management_token = response.data.access_token;
-  console.log("Management token:", management_token);
+  if (DEBUG) console.log("Management token:", management_token);
   return management_token;
 }
 
+//Get all auth0 users from this app
 async function get_users(token){
   const options = {
     method: 'GET',
@@ -90,10 +96,11 @@ async function get_users(token){
   }
   let response = await axios.request(options);
   const users = response.data;
-  console.log(users);
+  if (DEBUG) console.log(users);
   return users;
 }
 
+//Create auth0 user
 async function create_user(username, password, token){
   const options = {
     method: 'POST',
@@ -108,9 +115,10 @@ async function create_user(username, password, token){
     }
   }
   let response = await axios.request(options);
-  console.log(response.data);
+  if (DEBUG) console.log(response.data);
 }
 
+//Get auth0 user JWT token
 async function get_user_token(username, password){
   const options = {
     method: 'POST',
@@ -125,8 +133,8 @@ async function get_user_token(username, password){
     }
   }
   let response = await axios.request(options);
-  //console.log(response.data.id_token);
-  console.log(response.data);
+  //if (DEBUG) console.log(response.data.id_token);
+  if (DEBUG) console.log(response.data);
   return response.data.id_token;
 }
 
@@ -148,12 +156,24 @@ function attachID(entity){
   return entity;
 }
 
-//Returns all entities
-async function getEntities(kind){
-  let query = datastore.createQuery(kind);
-  return datastore.runQuery(query).then((entities) => {
-    return entities[0].map(attachID);
-  });
+//Get list of all entities, attach self url and id before returning
+//includes pagination (optional)
+async function getEntities(kind, filter, page_size, page_cursor) {
+
+  const query = datastore.createQuery(kind);
+
+  //Build query based on given parameters
+  if (page_cursor) query.start(page_cursor);
+  if (filter) query.filter(filter.property, filter.operator, filter.value);
+  if (page_size) query.limit(page_size);
+  if (DEBUG) console.log("Query:", query);
+
+  const results = await datastore.runQuery(query);
+  if (DEBUG) console.log("Results:", results);
+  const entities = results[0];
+  const info = results[1];
+
+  return [entities.map(attachID), info];
 }
 
 //Returns all public entities
@@ -163,7 +183,7 @@ async function getPublicEntities(kind){
   await datastore.runQuery(query).then((entities) => {
     all_entities = entities[0].map(attachID);
   });
-  //console.log("all entities:", all_entities);
+  //if (DEBUG) console.log("all entities:", all_entities);
   const private_entities = [];
   for (let i = 0; i < all_entities.length; i++){
     if (all_entities[i].public === true)
@@ -179,7 +199,7 @@ async function getOwnerEntities(kind, owner_id){
   await datastore.runQuery(query).then((entities) => {
     all_entities = entities[0].map(attachID);
   });
-  //console.log("all entities:", all_entities);
+  //if (DEBUG) console.log("all entities:", all_entities);
   const owner_entities = [];
   for (let i = 0; i < all_entities.length; i++){
     if (all_entities[i].owner === owner_id)
@@ -195,7 +215,7 @@ async function getOwnerPublicEntities(kind, owner_id){
   await datastore.runQuery(query).then((entities) => {
     all_entities = entities[0].map(attachID);
   });
-  console.log("all entities:", all_entities);
+  if (DEBUG) console.log("all entities:", all_entities);
   const owner_entities = [];
   for (let i = 0; i < all_entities.length; i++){
     if (all_entities[i].owner === owner_id &&
@@ -236,11 +256,47 @@ function getSelfURL(request, entity, kinds){
   return selfURL;
 }
 
+//Takes a request object and a array of accepted types of content
+function validateContentType(req, accepted_types){
+  if (DEBUG) console.log("Checking for valid content-types...");
+  
+  const accepts = req.accepts(accepted_types);
+  let error = null;
+  let status_code = null;
+  if (!accepts){
+    status_code = 406;
+    error = {"Error": "Not accepted response content-type. Must be application/json."};
+  }
+  
+  if (req.get('content-type') !== 'application/json'){
+    status_code = 415;
+    error = {"Error": "Invalid request content-type. Must be application/json."};
+  }
+  return [status_code, error];
+}
+
+//Checks if request body has valid data (eg. correct data type, length)
+function validateRequestData(req_body){
+  let error = null;
+  if (DEBUG) console.log("Checking if request body has valid data");
+
+  if  ((req_body.name && typeof req_body.name !== "string") ||
+       (req_body.type && typeof req_body.type !== "string") ||
+       (req_body.length && isNaN(req_body.length)))
+    error = {"Error": "One or more attribute has wrong data type."};
+  
+  else if ((req_body.name && req_body.name.length > 20) ||
+           (req_body.type && req_body.type.length > 15) ||
+           (req_body.length && req_body.length.length > 10))
+    error = {"Error": "One or more attribute has too many characters/digits."};
+  
+  return error;
+}
+
 //------------------------ROUTERS-----------------------------------
 
 
 //Render the home page
-
 app.get('/', function(req, res) { 
   res.render('home');
 });
@@ -260,13 +316,13 @@ app.post('/login', async function(req, res) {
     let found_user = false;
     for (let i = 0; i < users.length; i++){
       if (users[i].name === username){
-        console.log("found matching username")
+        if (DEBUG) console.log("found matching username")
         found_user = true;
       }
     }
     //If user doesn't exist, create one
     if (!found_user){
-      console.log("No matching user found");
+      if (DEBUG) console.log("No matching user found");
       await create_user(username, password, management_token);
     } 
 
@@ -277,7 +333,7 @@ app.post('/login', async function(req, res) {
     res.render('user_info', { jwt: user_jwt });
 
   } catch (error) {
-    console.log(error);
+    if (DEBUG) console.log(error);
     res.status(error.response.status).send(error.response.data);
   }
 });
@@ -286,18 +342,30 @@ app.post('/login', async function(req, res) {
 app.post('/boats', checkJwt, async function(req, res){
   
   try {
-    console.log(req.auth);
+
+    let status_code = null;
+    let error = null;
+
+    //Validate that request input data is valid
+    error = validateRequestData(req.body);
+    if (error){
+      if (DEBUG) console.log("Invalid request input data")
+      res.status(400).send(error);
+      return;
+    }
+
+    if (DEBUG) console.log(req.auth);
     const boat_info = req.body;
     boat_info.owner = req.auth.sub;
-    console.log("Adding boat:", req.body);
+    if (DEBUG) console.log("Adding boat:", req.body);
     const boat = await addEntity('boat', req.body);
     attachID(boat);
     attachSelfURL(req, boat, 'boats');
-    console.log("Boat added:", boat);
+    if (DEBUG) console.log("Boat added:", boat);
     res.status(201).send(boat);
 
   } catch (error) {
-    console.log(error);
+    if (DEBUG) console.log(error);
     res.send(error);
   }
 });
@@ -313,14 +381,44 @@ app.use(function (err, req, res, next) {
 });
 
 //Get all boats (public and private) for owner if valid JWT
-app.get('/owners/:owner_id/boats', checkJwt, async function(req, res){
+app.get('/owners/:owner_id/boats/:page_cursor?', checkJwt, async function(req, res){
   try{
-    const boats = await getOwnerEntities('boat', req.params.owner_id);  
-    console.log("All owner's boats:", boats);
+    //Set up query filter
+    const owner_id = req.params.owner_id;
+    const filter = {
+      property: "owner",
+      operator: "=",
+      value: owner_id
+    };  
+    let page_cursor = null;
+    if (req.params.page_cursor) page_cursor = req.params.page_cursor;
+    if (DEBUG) console.log("Page cursor:", page_cursor);
+    const page_size = 5;
+
+    //Get boat entities 
+    const results = await getEntities('boat', filter, page_size, page_cursor);
+    const boats = results[0];
+    page_cursor = results[1].endCursor;
+    if (DEBUG) console.log("Query results:", results);
+    if (DEBUG) console.log("Owner's boats:", boats);
     attachSelfURLs(req, boats, 'boats');
-    res.status(200).send(boats);
+
+    //Add next page link if there is one
+    let next_link = "NO MORE RESULTS";
+    if (results[1].moreResults !== Datastore.NO_MORE_RESULTS){
+      next_link = req.protocol + "://" + req.get('host') 
+                  + "/owners/" + owner_id + "/boats/"
+                  + encodeURIComponent(page_cursor);
+    }
+
+    const resp = {
+      "boats": boats,
+      "next": next_link
+    }
+    res.status(200).send(resp);
+
   } catch (error) {
-    console.log(error);
+    if (DEBUG) console.log(error);
     res.send(error);
   }
 });
@@ -329,10 +427,10 @@ app.get('/owners/:owner_id/boats', checkJwt, async function(req, res){
 app.use(async function (err, req, res, next) {
   console.error(err);
   if (err.name === "UnauthorizedError") {
-    console.log("bad jwt for get owner's boats request, return only owner's public boats");
+    if (DEBUG) console.log("bad jwt for get owner's boats request, return only owner's public boats");
     const owner_id = req.url.slice(8, 38);
     const boats = await getOwnerPublicEntities('boat', owner_id);
-    console.log("owner's public boats boats:", boats);
+    if (DEBUG) console.log("owner's public boats boats:", boats);
     attachSelfURLs(req, boats, 'boats');
     res.status(200).send(boats);
   } else {
@@ -345,11 +443,11 @@ app.get('/boats', checkJwt, async function(req, res) {
   try{
     const owner_id = req.auth.sub;
     const boats = await getOwnerEntities('boat', owner_id);
-    console.log("All owner's boats:", boats);
+    if (DEBUG) console.log("All owner's boats:", boats);
     attachSelfURLs(req, boats, 'boats');
     res.status(200).send(boats);
   } catch(error){
-    console.log(error);
+    if (DEBUG) console.log(error);
     res.send(error);
   }
 });
@@ -358,9 +456,9 @@ app.get('/boats', checkJwt, async function(req, res) {
 app.use(async function (err, req, res, next) {
   console.error(err);
   if (err.name === "UnauthorizedError") {
-    console.log("Invalid JWT token, responding with all public boats.")
+    if (DEBUG) console.log("Invalid JWT token, responding with all public boats.")
     const boats = await getPublicEntities('boat');
-    console.log("all public boats boats:", boats);
+    if (DEBUG) console.log("all public boats boats:", boats);
     attachSelfURLs(req, boats, 'boats');
     res.status(200).send(boats);
   } else {
@@ -373,12 +471,12 @@ app.delete('/boats/:boat_id', checkJwt, async function(req, res) {
   try{
     const boat_id = req.params.boat_id;
     const owner_id = req.auth.sub;
-    console.log("Looking for ID:", boat_id);
+    if (DEBUG) console.log("Looking for ID:", boat_id);
     const [boat] = await getEntity('boat', boat_id);
 
     //If no boat with ID found
     if (!boat){
-      console.log(`No boat with ID ${boat_id} found.`);
+      if (DEBUG) console.log(`No boat with ID ${boat_id} found.`);
       res
         .status(403)
         .set('Content-Type', 'application/json')
@@ -388,10 +486,10 @@ app.delete('/boats/:boat_id', checkJwt, async function(req, res) {
     }
     
     //If boat found, check if it belongs to this owner
-    console.log("Found boat:", boat);
-    console.log("checking if boat belongs to this owner...");
+    if (DEBUG) console.log("Found boat:", boat);
+    if (DEBUG) console.log("checking if boat belongs to this owner...");
     if (boat.owner !== owner_id){
-      console.log("Boat found, but belongs to different owner");
+      if (DEBUG) console.log("Boat found, but belongs to different owner");
       res
         .status(403)
         .set('Content-Type', 'application/json')
@@ -404,17 +502,17 @@ app.delete('/boats/:boat_id', checkJwt, async function(req, res) {
     const boat_key = boat[Datastore.KEY];
     datastore.delete(boat_key, (err, apiResp) => {
       if (err)
-        console.log(err);
-      console.log("Datastore delete api response:", apiResp);
+        if (DEBUG) console.log(err);
+      if (DEBUG) console.log("Datastore delete api response:", apiResp);
     });
-    console.log(`Boat ${boat_id} successfully deleted.`);
+    if (DEBUG) console.log(`Boat ${boat_id} successfully deleted.`);
     res
       .status(204)
       .set('Content-Type', 'application/json')
       .end();
 
   } catch(error){
-  console.log(error);
+  if (DEBUG) console.log(error);
   res.send(error);
   }
 });
@@ -423,7 +521,7 @@ app.delete('/boats/:boat_id', checkJwt, async function(req, res) {
 app.use(async function (err, req, res, next) {
   console.error(err);
   if (err.name === "UnauthorizedError") {
-    console.log("Invalid JWT token, delete request refused.")
+    if (DEBUG) console.log("Invalid JWT token, delete request refused.")
     res.status(401).send({"Error": "JWT invalid, delete request denied."});
   } else {
       next(err);
@@ -434,8 +532,8 @@ app.use(async function (err, req, res, next) {
 
 const PORT = parseInt(parseInt(process.env.PORT)) || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  if (DEBUG) console.log(`App listening on port ${PORT}`);
+  if (DEBUG) console.log('Press Ctrl+C to quit.');
 });
 
 module.exports = app;
